d17c14186bfacbc97ec113e927d428214e247c44
ce2cs <boyang.gao@yahoo.com>
2021-02-17 15:50:21 +0800

    finished proj2b

---
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -0,0 +1,159 @@
+package bearmaps;
+
+import edu.princeton.cs.algs4.In;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
+
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
+    private ArrayList<PriorityNode> minPQ;
+    private HashMap<T, Integer> indexMap;
+
+    public ArrayHeapMinPQ() {
+        this.minPQ = new ArrayList<>();
+        this.minPQ.add(new PriorityNode());
+        this.indexMap = new HashMap<>();
+    }
+
+    private class PriorityNode implements Comparable<PriorityNode> {
+        private double priority;
+        private T item;
+
+        public PriorityNode() {
+            this.priority = -1;
+            this.item = null;
+        }
+
+        public PriorityNode(double priority, T item) {
+            this.priority = priority;
+            this.item = item;
+        }
+
+        public double getPriority() {
+            return priority;
+        }
+
+        public T getItem() {
+            return item;
+        }
+
+        @Override
+        public int compareTo(PriorityNode n) {
+            if (n == null) {
+                return -1;
+            } else {
+                return Double.compare(this.getPriority(), n.getPriority());
+            }
+        }
+    }
+
+    @Override
+    public void add(T item, double priority) {
+        if (contains(item)) {
+            throw new IllegalArgumentException("item already exists");
+        }
+        this.minPQ.add(new PriorityNode(priority, item));
+        this.indexMap.put(item, this.size());
+        adjustForward(this.size());
+    }
+
+    private void adjustForward(int index) {
+        if (this.minPQ.get(getParentIndex(index)).getPriority() > this.minPQ.get(index).getPriority()) {
+            swap(getParentIndex(index), index);
+            adjustForward(getParentIndex(index));
+        }
+    }
+
+    private int getParentIndex(int index) {
+        return index / 2;
+    }
+
+    private void swap(int index1, int index2) {
+        this.indexMap.put(this.minPQ.get(index1).getItem(), index2);
+        this.indexMap.put(this.minPQ.get(index2).getItem(), index1);
+        PriorityNode tempNode = this.minPQ.get(index1);
+        this.minPQ.set(index1, this.minPQ.get(index2));
+        this.minPQ.set(index2, tempNode);
+    }
+
+    @Override
+    public boolean contains(T item) {
+        return this.indexMap.containsKey(item);
+    }
+
+    @Override
+    public T getSmallest() {
+        if (this.size() == 0) {
+            throw new NoSuchElementException("the heap is empty");
+        }
+        return this.minPQ.get(1).getItem();
+    }
+
+    @Override
+    public T removeSmallest() {
+        if (this.size() == 0) {
+            throw new NoSuchElementException("the heap is empty");
+        }
+        T ret = getSmallest();
+        this.minPQ.set(1, this.minPQ.get(size()));
+        this.minPQ.remove(this.size());
+        this.indexMap.remove(ret);
+        if (this.size() == 0) {
+            return ret;
+        }
+        adjustBackward(1);
+        return ret;
+    }
+
+    private void adjustBackward(int index) {
+        int leftChildIndex = getLeftChildIndex(index);
+        int rightChildIndex = getRightChildIndex(index);
+        int swapChildIndex = -1;
+        double currentPriority = this.minPQ.get(index).getPriority();
+        if (leftChildIndex > this.size()) {
+            return;
+        } else if (rightChildIndex > this.size()) {
+            if (currentPriority < this.minPQ.get(leftChildIndex).getPriority()) {
+                return;
+            } else {
+                swapChildIndex = leftChildIndex;
+            }
+        } else if (currentPriority < Math.min(this.minPQ.get(leftChildIndex).getPriority(), this.minPQ.get(rightChildIndex).getPriority())) {
+            return;
+        } else if (this.minPQ.get(leftChildIndex).getPriority() < this.minPQ.get(rightChildIndex).getPriority()) {
+            swapChildIndex = leftChildIndex;
+        } else {
+            swapChildIndex = rightChildIndex;
+        }
+        if (swapChildIndex > 0) {
+            swap(swapChildIndex, index);
+            adjustBackward(swapChildIndex);
+        }
+    }
+
+    private int getLeftChildIndex(int index) {
+        return index * 2;
+    }
+
+    private int getRightChildIndex(int index) {
+        return index * 2 + 1;
+    }
+
+
+    @Override
+    public int size() {
+        return this.minPQ.size() - 1;
+    }
+
+    @Override
+    public void changePriority(T item, double priority) {
+        if (!contains(item)) {
+            throw new NoSuchElementException("no such item");
+        }
+        int changeIndex = this.indexMap.get(item);
+        this.minPQ.set(changeIndex, new PriorityNode(priority, item));
+        adjustBackward(changeIndex);
+        adjustForward(changeIndex);
+    }
+}
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,7 +1,111 @@
 package bearmaps;
 
+import org.junit.Assert;
 import org.junit.Test;
+import edu.princeton.cs.algs4.Stopwatch;
+
 import static org.junit.Assert.assertEquals;
 
+import java.util.Locale;
+import java.util.Random;
+
 public class ArrayHeapMinPQTest {
+    ArrayHeapMinPQ<Integer> heapq = new ArrayHeapMinPQ<>();
+    NaiveMinPQ<Integer> naiveMinPQ = new NaiveMinPQ<>();
+
+    @Test
+    public void testAdd() {
+        heapq.add(1, 1);
+        heapq.add(2, 3);
+        heapq.add(3, 10);
+        heapq.add(4, 1);
+        heapq.add(5, 1);
+        heapq.add(6, 0);
+    }
+
+    @Test
+    public void testRemove() {
+        Random randomGenerator = new Random(0);
+        int caseNumber = 10000;
+        for (int i = 0; i < caseNumber; i++) {
+            double priority = 10000 * randomGenerator.nextDouble();
+            heapq.add(i, priority);
+            naiveMinPQ.add(i, priority);
+        }
+        for (int i = 0; i < caseNumber; i++) {
+            assertEquals(heapq.removeSmallest(), naiveMinPQ.removeSmallest());
+        }
+    }
+
+    @Test
+    public void testChangePriority() {
+        Random randomGenerator = new Random(4);
+        int caseNumber = 10000;
+        for (int i = 0; i < caseNumber; i++) {
+            double priority = 10000 * randomGenerator.nextDouble();
+            heapq.add(i, priority);
+            naiveMinPQ.add(i, priority);
+            if (i % 2 == 0) {
+                priority = 10000 * randomGenerator.nextDouble();
+                heapq.changePriority(i, priority);
+                naiveMinPQ.changePriority(i, priority);
+            }
+            if (i % 3 == 0) {
+                priority = 10000 * randomGenerator.nextDouble();
+                heapq.changePriority(i, priority);
+                naiveMinPQ.changePriority(i, priority);
+            }
+        }
+        for (int i = 1; i < caseNumber; i++) {
+            assertEquals(heapq.contains(i), naiveMinPQ.contains(i));
+            assertEquals(heapq.getSmallest(), naiveMinPQ.getSmallest());
+            assertEquals(heapq.size(), naiveMinPQ.size());
+            assertEquals(heapq.removeSmallest(), naiveMinPQ.removeSmallest());
+            assertEquals(heapq.contains(i), naiveMinPQ.contains(i));
+            assertEquals(heapq.getSmallest(), naiveMinPQ.getSmallest());
+            assertEquals(heapq.size(), naiveMinPQ.size());
+        }
+    }
+
+    @Test
+    public void compareSpeed() {
+        Stopwatch timer1 = new Stopwatch();
+        Random randomGenerator = new Random(4);
+        int caseNumber = 100000;
+        for (int i = 0; i < caseNumber; i++) {
+            double priority = 10000 * randomGenerator.nextDouble();
+            heapq.add(i, priority);
+            if (i % 2 == 0) {
+                priority = 10000 * randomGenerator.nextDouble();
+                heapq.changePriority(i, priority);
+            }
+            if (i % 3 == 0) {
+                priority = 10000 * randomGenerator.nextDouble();
+                heapq.changePriority(i, priority);
+            }
+        }
+        for (int i = 0; i < caseNumber; i++) {
+            heapq.removeSmallest();
+        }
+        double time1 = timer1.elapsedTime();
+
+        Stopwatch timer2 = new Stopwatch();
+        for (int i = 0; i < caseNumber; i++) {
+            double priority = 10000 * randomGenerator.nextDouble();
+            naiveMinPQ.add(i, priority);
+            if (i % 2 == 0) {
+                priority = 10000 * randomGenerator.nextDouble();
+                naiveMinPQ.changePriority(i, priority);
+            }
+            if (i % 3 == 0) {
+                priority = 10000 * randomGenerator.nextDouble();
+                naiveMinPQ.changePriority(i, priority);
+            }
+        }
+        for (int i = 0; i < caseNumber; i++) {
+            naiveMinPQ.removeSmallest();
+        }
+        double time2 = timer2.elapsedTime();
+        System.out.println(String.format("%f, %f", time1, time2));
+    }
 }
