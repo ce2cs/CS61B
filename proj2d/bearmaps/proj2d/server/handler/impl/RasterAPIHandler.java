499ca082b272514e3b5d8fe98ccb0d1eb6d817e1
ce2cs <boyang.gao@yahoo.com>
2021-03-01 19:25:41 +0800

    finished proj2d part1

--- proj2d/bearmaps/proj2d/server/handler/impl/RasterAPIHandler.java
+++ proj2d/bearmaps/proj2d/server/handler/impl/RasterAPIHandler.java
@@ -2,6 +2,8 @@ package bearmaps.proj2d.server.handler.impl;
 
 import bearmaps.proj2d.AugmentedStreetMapGraph;
 import bearmaps.proj2d.server.handler.APIRouteHandler;
+import bearmaps.proj2d.utils.Tuple;
+import edu.princeton.cs.algs4.In;
 import spark.Request;
 import spark.Response;
 import bearmaps.proj2d.utils.Constants;
@@ -17,12 +19,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static bearmaps.proj2d.utils.Constants.SEMANTIC_STREET_GRAPH;
-import static bearmaps.proj2d.utils.Constants.ROUTE_LIST;
+import static bearmaps.proj2d.utils.Constants.*;
 
 /**
  * Handles requests from the web browser for map images. These images
  * will be rastered into one large image to be displayed to the user.
+ *
  * @author rahul, Josh Hug, _________
  */
 public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<String, Object>> {
@@ -54,22 +56,21 @@ public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<S
     /**
      * Takes a user query and finds the grid of images that best matches the query. These
      * images will be combined into one big image (rastered) by the front end. <br>
-     *
-     *     The grid of images must obey the following properties, where image in the
-     *     grid is referred to as a "tile".
-     *     <ul>
-     *         <li>The tiles collected must cover the most longitudinal distance per pixel
-     *         (LonDPP) possible, while still covering less than or equal to the amount of
-     *         longitudinal distance per pixel in the query box for the user viewport size. </li>
-     *         <li>Contains all tiles that intersect the query bounding box that fulfill the
-     *         above condition.</li>
-     *         <li>The tiles must be arranged in-order to reconstruct the full image.</li>
-     *     </ul>
+     * <p>
+     * The grid of images must obey the following properties, where image in the
+     *  is referred to as a "tile".
+     * <ul>
+     *     <li>The tiles collected must cover the most longitudinal distance per pixel
+     *     (LonDPP) possible, while still covering less than or equal to the amount of
+     *     longitudinal distance per pixel in the query box for the user viewport size. </li>
+     *     <li>Contains all tiles that intersect the query bounding box that fulfill the
+     *     above condition.</li>
+     *     <li>The tiles must be arranged in-order to reconstruct the full image.</li>
+     * </ul>
      *
      * @param requestParams Map of the HTTP GET request's query parameters - the query box and
-     *               the user viewport width and height.
-     *
-     * @param response : Not used by this function. You may ignore.
+     *                      the user viewport width and height.
+     * @param response      : Not used by this function. You may ignore.
      * @return A map of results for the front end as specified: <br>
      * "render_grid"   : String[][], the files to display. <br>
      * "raster_ul_lon" : Number, the bounding upper left longitude of the rastered image. <br>
@@ -77,19 +78,167 @@ public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<S
      * "raster_lr_lon" : Number, the bounding lower right longitude of the rastered image. <br>
      * "raster_lr_lat" : Number, the bounding lower right latitude of the rastered image. <br>
      * "depth"         : Number, the depth of the nodes of the rastered image;
-     *                    can also be interpreted as the length of the numbers in the image
-     *                    string. <br>
+     * can also be interpreted as the length of the numbers in the image
+     * string. <br>
      * "query_success" : Boolean, whether the query was able to successfully complete; don't
-     *                    forget to set this to true on success! <br>
+     * forget to set this to true on success! <br>
      */
     @Override
     public Map<String, Object> processRequest(Map<String, Double> requestParams, Response response) {
-        //System.out.println("yo, wanna know the parameters given by the web browser? They are:");
-        //System.out.println(requestParams);
+        System.out.println(requestParams);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented RasterAPIHandler.processRequest, nothing is displayed in "
-                + "your browser.");
+        double lrlon = requestParams.get("lrlon");
+        double ullon = requestParams.get("ullon");
+        double lrlat = requestParams.get("lrlat");
+        double ullat = requestParams.get("ullat");
+        double w = requestParams.get("w");
+        double h = requestParams.get("h");
+        if (!checkBoundingBox(lrlon, lrlat, ullon, ullat)) {
+            results.put("query_success", false);
+            return results;
+        }
+        double lonDPP = calLonDPP(lrlon, ullon, w);
+        int zoomTimes = getZoomTimes(lonDPP);
+        Tuple<Integer, Integer> lrImageIndex = getImageIndex(lrlon, lrlat, zoomTimes);
+        Tuple<Integer, Integer> ulImageIndex = getImageIndex(ullon, ullat, zoomTimes);
+        String[][] displayImages = getDisplayImages(lrImageIndex, ulImageIndex, zoomTimes);
+        int divisionNum = (int) Math.pow(2, zoomTimes);
+        double boundingBoxULLon = getLonByIndex(ulImageIndex.getFirst(), divisionNum);
+        double boundingBoxULLat = getLatByIndex(ulImageIndex.getSecond(), divisionNum);
+        double boundingBoxLRLon = getLonByIndex(lrImageIndex.getFirst() + 1, divisionNum);
+        double boundingBoxLRLat = getLatByIndex(lrImageIndex.getSecond() + 1, divisionNum);
+        results.put("render_grid", displayImages);
+        results.put("raster_ul_lon", boundingBoxULLon);
+        results.put("raster_ul_lat", boundingBoxULLat);
+        results.put("raster_lr_lon", boundingBoxLRLon);
+        results.put("raster_lr_lat", boundingBoxLRLat);
+        results.put("depth", zoomTimes);
+        results.put("query_success", true);
+        System.out.println(results);
         return results;
+    }
+    private boolean checkBoundingBox(double lrlon, double lrlat, double ullon, double ullat) {
+        if (lrlat > ullat) {
+            return false;
+        }
+        if (lrlon < ullon) {
+            return false;
+        }
+        return inMap(lrlon, lrlat) | inMap(ullon, ullat);
+    }
+
+    private boolean inMap(double lon, double lat) {
+        return (lon < ROOT_LRLON && lon > ROOT_ULLON && lat > ROOT_LRLAT && lat < ROOT_ULLAT);
+    }
+
+    private double calLonDPP(double lrlon, double ullon, double w) {
+        return (lrlon - ullon) / w;
+    }
+
+    private int getZoomTimes(double lonDPP) {
+        double d0LonDPP = calLonDPP(Constants.ROOT_LRLON, Constants.ROOT_ULLON, Constants.TILE_SIZE);
+        int zoomTimes = 0;
+        while (d0LonDPP > lonDPP) {
+            d0LonDPP = d0LonDPP / 2.;
+            zoomTimes += 1;
+        }
+        if (zoomTimes >= 7) {
+            zoomTimes = 7;
+        }
+        return zoomTimes;
+    }
+
+    private String[][] getDisplayImages(Tuple<Integer, Integer> lrImageIndex, Tuple<Integer, Integer> ulImageIndex, int zoomTimes) {
+        int lonImageNum = lrImageIndex.getFirst() - ulImageIndex.getFirst() + 1;
+        int latImageNum = lrImageIndex.getSecond() - ulImageIndex.getSecond() + 1;
+        String[][] retStringArray = new String[latImageNum][lonImageNum];
+        for (int i = 0; i < latImageNum; i += 1) {
+            for (int j = 0; j < lonImageNum; j += 1) {
+                int currentLonIndex = ulImageIndex.getFirst() + j;
+                int currentLatIndex = ulImageIndex.getSecond() +  i;
+                retStringArray[i][j] = "d" + zoomTimes + "_x" + currentLonIndex + "_y" + currentLatIndex + ".png";
+            }
+        }
+        return retStringArray;
+    }
+
+    private Tuple<Integer, Integer> getImageIndex(double lon, double lat, int zoomTimes) {
+        int divideNum = (int) Math.pow(2, zoomTimes);
+        int lonIndex = getIndexByLon(lon, divideNum);
+        int latIndex = getIndexByLat(lat, divideNum);
+        return new Tuple<>(lonIndex, latIndex);
+    }
+
+    private int getIndexByLon(double lon, int divideNum) {
+        return lonDichotomy(lon, divideNum);
+    }
+
+    private int getIndexByLat(double lat, int divideNum) {
+        return latDichotomy(lat, divideNum);
+    }
+
+    private int lonDichotomy(double coor, int divideNum) {
+        int midIndex = divideNum / 2;
+        int currentUpperIndex = divideNum;
+        int currentLowerIndex = 0;
+        while (currentUpperIndex - currentLowerIndex > 1) {
+            double midCoor = getLonByIndex(midIndex, divideNum);
+            double midRightCoor = getLonByIndex(midIndex + 1, divideNum);
+            double midLeftCoor = getLonByIndex(midIndex - 1, divideNum);
+            if (coor > midCoor) {
+                if (coor < midRightCoor) {
+                    return midIndex;
+                } else {
+                    currentLowerIndex = midIndex;
+                    midIndex = currentLowerIndex + (currentUpperIndex - currentLowerIndex)/ 2;
+                }
+            } else if (coor < midCoor) {
+                if (coor > midLeftCoor) {
+                    return midIndex - 1;
+                } else {
+                    currentUpperIndex = midIndex;
+                    midIndex = currentLowerIndex + (currentUpperIndex - currentLowerIndex)/ 2;
+                }
+            }
+        }
+        return midIndex;
+    }
+
+    private int latDichotomy(double coor, int divideNum) {
+        int currentUpperIndex = 0;
+        int currentLowerIndex = divideNum;
+        int midIndex = (currentLowerIndex - currentUpperIndex) / 2;
+        while (currentLowerIndex - currentUpperIndex > 1) {
+            double midCoor = getLatByIndex(midIndex, divideNum);
+            double midUpCoor = getLatByIndex(midIndex - 1, divideNum);
+            double midDownCoor = getLatByIndex(midIndex + 1, divideNum);
+            if (coor > midCoor) {
+                if (coor < midUpCoor) {
+                    return midIndex - 1;
+                } else  {
+                    currentLowerIndex = midIndex;
+                    midIndex = currentUpperIndex + (currentLowerIndex - currentUpperIndex) / 2;
+                }
+            } else if (coor < midCoor) {
+                if (coor > midDownCoor) {
+                    return midIndex;
+                } else {
+                    currentUpperIndex = midIndex;
+                    midIndex = currentUpperIndex + (currentLowerIndex - currentUpperIndex);
+                }
+            }
+        }
+        return midIndex;
+    }
+
+    private double getLonByIndex(double index, int divideNum) {
+        double coorCoveragePerDivision = (ROOT_LRLON - ROOT_ULLON) / divideNum;
+        return coorCoveragePerDivision * index + ROOT_ULLON;
+    }
+
+    private double getLatByIndex(double index,  int divideNum) {
+        double coorCoveragePerDivision = (ROOT_LRLAT - ROOT_ULLAT) / divideNum;
+        return coorCoveragePerDivision * index + ROOT_ULLAT;
     }
 
     @Override
@@ -119,6 +268,7 @@ public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<S
 
     /**
      * Validates that Rasterer has returned a result that can be rendered.
+     *
      * @param rip : Parameters provided by the rasterer
      */
     private boolean validateRasteredImgParams(Map<String, Object> rip) {
@@ -143,8 +293,8 @@ public class RasterAPIHandler extends APIRouteHandler<Map<String, Double>, Map<S
      * In Spring 2016, students had to do this on their own, but in 2017,
      * we made this into provided code since it was just a bit too low level.
      */
-    private  void writeImagesToOutputStream(Map<String, Object> rasteredImageParams,
-                                                  ByteArrayOutputStream os) {
+    private void writeImagesToOutputStream(Map<String, Object> rasteredImageParams,
+                                           ByteArrayOutputStream os) {
         String[][] renderGrid = (String[][]) rasteredImageParams.get("render_grid");
         int numVertTiles = renderGrid.length;
         int numHorizTiles = renderGrid[0].length;
